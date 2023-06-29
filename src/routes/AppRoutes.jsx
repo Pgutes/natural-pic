11b84f1791ddfa85dea1cb9775d00e67abf8633c
pgutes <pedrod.gutes@gmail.com>
2023-06-29 15:34:47 -0400

    primer commit

---
+++ src/routes/AppRoutes.jsx
@@ -0,0 +1,14 @@
+import {Route, Routes} from "react-router-dom";
+import Home from "../views/Home";
+import Favoritos from "../views/Favoritos";
+
+const AppRoutes = () => {
+    return (
+        <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/favoritos" element={<Favoritos />} />
+        </Routes>
+    );
+}
+
+export default AppRoutes;
